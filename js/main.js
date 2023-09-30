59a0e056a36eae44d9deb7810539da7e1ac5d790
Mo Essam <mohameddessam303@gmail.com>
2023-09-30 18:51:44 +0300

    modyfing code

--- js/main.js
+++ js/main.js
@@ -1,65 +1,89 @@
 // Page dosen't show untill animation done
-var main = document.getElementsByTagName('main')[0];
-main.style.display='none';
-setTimeout(()=>{
-    main.style.display='block';
-},2490)
+let main = document.getElementsByTagName("main")[0];
+main.style.display = "none";
+setTimeout(() => {
+  main.style.display = "block";
+}, 2490);
 
 // Carousel
-var target;
-var list = document.getElementsByClassName('overlay-list');
-var carousel = document.getElementsByClassName('carousel-item');
+let target;
+let list = document.getElementsByClassName("overlay-list");
+let carousel = document.getElementsByClassName("carousel-item");
 // navbar change
-var li = document.querySelectorAll('.nav-item a');
-li[0].addEventListener('click',()=>{
-    removeClass()
-    li[0].classList.add('active')
-})
-li[1].addEventListener('click',()=>{
-    removeClass()
-    li[1].classList.add('active')
-})
-li[2].addEventListener('click',()=>{
-    removeClass()
-    li[2].classList.add('active')
-})
-li[3].addEventListener('click',()=>{
-    removeClass()
-    li[3].classList.add('active')
-})
-li[4].addEventListener('click',()=>{
-    removeClass()
-    li[4].classList.add('active')
-})
-li[5].addEventListener('click',()=>{
-    removeClass()
-    li[5].classList.add('active')
-})
-li[6].addEventListener('click',()=>{
-    removeClass()
-    li[6].classList.add('active')
-})
+let li = document.querySelectorAll(".nav-item a");
+li[0].addEventListener("click", () => {
+  removeClass();
+  li[0].classList.add("active");
+});
+li[1].addEventListener("click", () => {
+  removeClass();
+  li[1].classList.add("active");
+});
+li[2].addEventListener("click", () => {
+  removeClass();
+  li[2].classList.add("active");
+});
+li[3].addEventListener("click", () => {
+  removeClass();
+  li[3].classList.add("active");
+});
+li[4].addEventListener("click", () => {
+  removeClass();
+  li[4].classList.add("active");
+});
+li[5].addEventListener("click", () => {
+  removeClass();
+  li[5].classList.add("active");
+});
+li[6].addEventListener("click", () => {
+  removeClass();
+  li[6].classList.add("active");
+});
 function removeClass() {
-    for (let i = 0; i < li.length; i++) {
-        li[i].classList.remove('active');
-    }
+  for (let i = 0; i < li.length; i++) {
+    li[i].classList.remove("active");
+  }
 }
 
+// Nav Bar
+let nav = document.querySelector(".navbar");
 
+window.onscroll = function () {
+  if (window.scrollY >= 700) {
+    nav.classList.add("dark");
+  } else {
+    nav.classList.remove("dark");
+  }
+};
 
+let iconContainer = document.querySelector(".icon-container");
+let toggle = false;
 
-
-
-
-
-
-
-
-
-
-
-
-
+iconContainer.addEventListener("click", () => {
+  if (toggle === false) {
+    document.querySelector(".menu-icon span:first-child").style = `
+        top: 6px; transform: rotate(45deg);
+      `;
+    document.querySelector(".menu-icon span:nth-child(2)").style = `
+      opacity: 0;
+    `;
+    document.querySelector(".menu-icon span:last-child").style = `
+      top: 6px;transform: rotate(-45deg);
+    `;
+    toggle = !toggle;
+  } else {
+    document.querySelector(".menu-icon span:first-child").style = `
+      top: 0px; transform: rotate(0deg);
+    `;
+    document.querySelector(".menu-icon span:nth-child(2)").style = `
+      opacity: 1;
+    `;
+    document.querySelector(".menu-icon span:last-child").style = `
+      top: 12px;transform: rotate(0deg);
+    `;
+    toggle = !toggle;
+  }
+});
 
 // setInterval(()=>{
 //     checkClass()
@@ -72,11 +96,11 @@ function removeClass() {
 //         if (carousel[i].classList.contains('active')) {
 //             target=i;
 //         }
-        
+
 //     }
 //     for (let i = 0; i < list.length; i++) {
 //         list[i].classList.remove('list-active');
-        
+
 //     }
 //     list[target].classList.add('list-active')
 // }
